fd7b52312a1f6f81b21723085e4d47e02bc9b6b0
Rajat Sudagade <rajat.k16@gmail.com>
2019-08-25 20:21:53 -0500

    removed comments

--- script.py
+++ script.py
@@ -11,25 +11,25 @@ pd.set_option('display.width', 1000)
 
 # load the dataset
 beers = pd.read_csv('./beers.csv')
-# print(beers.head(10))
+print(beers.head(10))
 
 # Describe data
-# print(beers.describe())
-# print(beers.info())
+print(beers.describe())
+print(beers.info())
 
 # types of beers
-# print((beers["style"].value_counts()).head(10))
+print((beers["style"].value_counts()).head(10))
 
 # Countplot
-# sns.countplot(x='style', data=beers)
-# sns.countplot(x="ounces", data=beers)
+sns.countplot(x='style', data=beers)
+sns.countplot(x="ounces", data=beers)
 
 # Scatter Plot
-# sns.FacetGrid(beers, hue="ounces").map(plt.scatter, 'ibu', 'abv').add_legend()
+sns.FacetGrid(beers, hue="ounces").map(plt.scatter, 'ibu', 'abv').add_legend()
 
 # Box Plot
-# ax = sns.boxplot(x="ounces", y="abv", data=beers)
-# ax = sns.stripplot(x="ounces", y="abv", data=beers, jitter=True, edgecolor="gray")
+ax = sns.boxplot(x="ounces", y="abv", data=beers)
+ax = sns.stripplot(x="ounces", y="abv", data=beers, jitter=True, edgecolor="gray")
 
 # # Violin Plot
 sns.violinplot(x="ounces", y="abv", data=beers, size=6)
